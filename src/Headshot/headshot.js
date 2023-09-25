dc9fc3c9709e57ae52f8bcfe55548fd572ba20dd
rlondon3 <codesmithr@outlook.com>
2023-09-24 22:52:51 -0700

    add summary, more skills, and styling.

--- src/Headshot/headshot.js
+++ src/Headshot/headshot.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { Avatar, Chip, Divider } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
@@ -13,23 +14,45 @@ import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import RssFeedIcon from '@mui/icons-material/RssFeed';
+import Box from '@mui/material/Box';
 import avatar from '../assets/avatar.png';
+
 
 const mock = [
   {
     name: 'SKILLS:',
-    tags: ['HTML', 'CSS', 'JavaScript', 'React',  'NodeJS', 'Python', 'Mongo', 'Postgresql', 'AWS Cloud Services', 'Unit Testing', 'CI/CD', 'Power BI'],
+    tags: ['HTML', 'CSS', 'JavaScript', 'React',  'NodeJS', 'Python', 'No SQL', 
+      'SQL', 'AWS Cloud Services', 'Unit Testing', 'CI/CD', 
+      'Power BI', 'Jupyter Notebook', 'Visual Studio', 'Visual Studio Code',
+      'Business Metrics/Data', 'Self-Starter', 'Creative Problem Solver'],
     avatar: avatar
   },
 ];
 
 const HeadShot= () => {
+  const theme = useTheme();
+  
+  const isXs = useMediaQuery(theme.breakpoints.up('xs'), {
+    defaultMatches: true,
+  });
+
   return (
     <Container>
       <Box>
-        <Box marginBottom={4}>
+        <Box>
           < Typography fontWeight={700} variant={'h4'} align={'center'}>
             Raphael Smith
+          </Typography>
+          <Typography
+            sx={{
+              textTransform: 'uppercase',
+            }}
+            gutterBottom
+            color={'text.secondary'}
+            align={'center'}
+            fontWeight={700}
+          >
+            <Typewriter text="Full-stack Software Engineer | Junior Data Scientist" delay={100} />
           </Typography>
           <Divider />
           <p style={{ textAlign: 'center'}}>
@@ -41,62 +64,51 @@ const HeadShot= () => {
             <a href='https://programmingthestreets.hashnode.dev/' target='_blank' rel='noopener noreferrer'><RssFeedIcon /></a>
           </p>
         </Box>
-        <Grid container spacing={2}>
+        <br />
+        <Grid container spacing={2} >
           {mock.map((item, i) => (
-            <Grid item xs={12} sm={8} md={8} lg={6} key={i}>
-              <Card
-                sx={{
-                  boxShadow: 0,
-                  background: 'transparent',
-                  backgroundImage: 'none',
-                }}
-              >
-                <Box
+            <Grid item xs={12} md={6} key={i} >
+              <Card sx={{
+                boxShadow: 0,
+                background: 'transparent',
+                backgroundImage: 'none',
+              }}>
+                <Box padding={5} xs={12} md={6}
                   component={Avatar}
-                  width={{ xs: 280, sm: 280, md: 320 }}
-                  height={{ xs: 280, sm: 280, md: 320 }}
+                  width={{ xs: 320, sm: 440, md: 320 }}
+                  height={{ xs: 320, sm: 440, md: 320 }}
+                  left={(isXs ? 60 : '')}
                   src={item.avatar}
                 />
-                <Box
-                  component={CardContent}
-                  bgcolor={'transparent'}
-                  marginTop={-5}
-                >
-                  <Box component={Card} paddingTop={3}>
-                    <Typography
-                      sx={{
-                        textTransform: 'uppercase',
-                      }}
-                      gutterBottom
-                      color={'text.secondary'}
-                      align={'center'}
-                      fontWeight={700}
-                    >
-                      <Typewriter text="Full-stack Software Engineer | Junior Data Scientist" delay={100} />
-                    </Typography>
-                    <CardContent>
-                      {item.tags.map((item) => (
-                        <Chip
-                          key={item}
-                          label={item}
-                          component="a"
-                          href=""
-                          size={'small'}
-                          sx={{ marginBottom: 1, marginRight: 1 }}
-                        />
-                      ))}
-                    </CardContent>
-                  </Box>
-                </Box>
               </Card>
             </Grid>
           ))}
-          <Grid p={5} xs={12} sm={8} md={8} lg={6} style={{alignSelf: 'center'}} >
+          <Grid paddingTop={5} xs={12} md={6} style={{alignSelf: 'center'}} >
             <Card sx={{
               boxShadow: 0,
               background: 'transparent',
               backgroundImage: 'none',
             }}>
+              <Box component={Card}>
+                <Typography variant='h5'><b>Summary</b></Typography>
+                <p>Proven developer with a trackable interest in data science and 3 years of experience building software to solve business
+                  problems. Detailed, thorough, and reliably meet deadlines in demanding and fast paced environments. Quick learner, energetic 
+                  collaborator, and advocate of fun and hardwork on the job. 
+                </p>
+                <Typography variant='h5'><b>Skills</b></Typography>
+                <CardContent>
+                  {mock[0].tags.map((item) => (
+                    <Chip
+                      key={item}
+                      label={item}
+                      component="a"
+                      href=""
+                      size={'small'}
+                      sx={{ marginBottom: 1, marginRight: 1 }}
+                    />
+                  ))}
+                </CardContent>
+              </Box>
             </Card>
           </Grid>
         </Grid>
--- src/WithThreeColumns/WithThreeColumns.js
+++ src/WithThreeColumns/WithThreeColumns.js
@@ -2,27 +2,10 @@ import React from 'react';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import Container from 'components/Container';
 import HeadShot from 'Headshot/headshot';
 import { Typography } from '@mui/material';
 import Strengths from 'Strengths';
-
-const ChildMock = () => {
-  const theme = useTheme();
-  return (
-    <Box
-      width={1}
-      height={1}
-      minHeight={{ xs: 400, md: 800 }}
-      borderRadius={2}
-      border={`2px solid ${theme.palette.divider}`}
-      sx={{
-        borderStyle: 'dashed',
-      }}
-    />
-  );
-};
 
 const WithThreeColumns = () => {
   const theme = useTheme();
@@ -32,35 +15,15 @@ const WithThreeColumns = () => {
 
   return (
     <Box>
-      <Container maxWidth={1} paddingY={{ xs: 1, sm: 1.5 }}>
+      <Container>
         <Typography variant='h6' color='grey'>Cloud Resume</Typography>
       </Container>
       <HeadShot
         variant={isMd ? 'permanent' : 'temporary'}
       />
-      <main>
-        <Box height={{ xs: 58, sm: 66, md: 71 }} />
-        <Box
-          display="flex"
-          flex="1 1 auto"
-          overflow="hidden"
-        >
-          <Box display="flex" flex="1 1 auto" overflow="hidden">
-            <Box flex="1 1 auto" height="100%" overflow="auto">
-              <Box p={4}>
-                <Grid container spacing={4}>
-                  <Grid item xs={12} md={6}>
-                    <Strengths />
-                  </Grid>
-                  <Grid item xs={12} md={6}>
-                    <ChildMock />
-                  </Grid>
-                </Grid>
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-      </main>
+      <Box>
+        <Strengths />
+      </Box>
     </Box>
   );
 };
